d4513adf4100e1cd4e7a077f4fd9c61766cf009a
Maria Lara Rossetti <marialararossetti@gmail.com>
2024-11-20 08:33:06 +0100

    flf

--- Streamlit/pagina7.py
+++ Streamlit/pagina7.py
@@ -1,33 +1,31 @@
 #Pagina del flujo de fondos
 import streamlit as st
 import pandas as pd 
+import numpy_financial as npf
 
 st.title('Flujo de fondos')
 
-# Dataset de VE
-# df_autos = pd.read_csv("df_modelo.csv") # Dataset de VE
-df_autos = pd.read_csv('datasets/2. Depurados/ElectricCarData_Clean.csv')
-#"df_modelo.csv")
-df_autos.rename(columns={
-    'model': 'Model',
-    'efficiency_whkm': 'Efficiency (Wh/km)'
-}, inplace=True)
-
-df_autos['Efficiency (kWh/mile)'] = df_autos['Efficiency (Wh/km)']/1000* 0.1688666667 
-df_autos['Precio Dolar'] = df_autos['price_euro'] * 1.06
-
-#future_data = pd.read_csv(r"Prediccion_Dataset.csv", index_col = 0) # Resultados del modelo ML (pronostico a 5 años)
+#Carga de datasets
 future_data = pd.read_csv('datasets/2. Depurados/TLC Aggregated Data/ML_TS_Output_Anualized.csv')
 future_data = future_data[future_data['industry']=='FHV - High Volume']
 
 future_data.rename(columns={
     'anual_income_per_vehicle': 'Income_per_Vehicle (USD)',
     'anual_distance_per_vehicle': 'Miles_per_Vehicle',
-    #'anual_hours_per_driver': 'Hours_per_Driver',
-    'anual_total_co2_emissio': 'Total_CO2_Emissions'
+    'anual_total_co2_emission': 'Total_CO2_Emissions'
 }, inplace=True)
-#anual_hours_per_driver = future_data[future_data['year']<2025]['Hours_per_Driver'].mean()
 future_data =  future_data[future_data['year']>=2025]
+
+# Dataset de VE
+df_autos = pd.read_csv('datasets/2. Depurados/ElectricCarData_Clean.csv')#"df_modelo.csv")
+df_autos.rename(columns={
+    'brand': 'Brand',
+    'model': 'Model',
+    'efficiency_whkm': 'Efficiency (Wh/km)'
+}, inplace=True)
+
+df_autos['Efficiency (kWh/mile)'] = df_autos['Efficiency (Wh/km)']/1000* 0.1688666667 
+df_autos['Precio Dolar'] = df_autos['price_euro'] * 1.06
 
 promedio_ingresos = round(future_data["Income_per_Vehicle (USD)"].mean(),2)
 promedio_millas = round(future_data["Miles_per_Vehicle"].mean(),2)
@@ -56,8 +54,11 @@ for _, row in future_data.iterrows():
     millas_años.append(round(row["Miles_per_Vehicle"],2))
 #print(millas_años)
 
+#Creacion de clase AUTO
+
 class Auto:
-    def __init__(self, precio, iva, licencia_1, licencia_2, placa, inspeccion, seguro, recorrido_anual, ingresos_anuales, tasa_descuento):
+    def __init__(self,tasa_descuento, precio, iva, licencia_1, licencia_2, placa, inspeccion, seguro, recorrido_anual, 
+                 ingresos_anuales):
         self.precio = precio
         self.iva = iva
         self.licencia_1 = licencia_1
@@ -78,19 +79,21 @@ class Auto:
     def flujo_neto_anual(self):
         return self.ingresos_anuales - self.costos_operativos()
     
-    def flujo_caja_proyectado(self, años=5):
+    def flujo_caja_proyectado(self, años=7):
+        
         # Inversión inicial negativa porqué es el año 0
         inversion_inicial = -self.inversion_inicial()
         
         ingresos_año = [0]
         costos_año = [0]
-        flujo_neto = [0]  # Año 0: 
+        flujo_neto = [inversion_inicial]  # Año 0: 
         flujo_descuento = [inversion_inicial]  # Año 0 Inversión inicial negativa
         ingresos_descuento_año = [0]
-        # Se calcula el flujo neto anual y mensual para cada año
+        
+        # Se calcula el flujo neto anual
         for año in range(1, años + 1):
-            self.ingresos_anuales = ingresos_años[año-1] # Codigo nuevo
-            self.recorrido_anual = millas_años[año-1] # Codigo nuevo
+            self.ingresos_anuales = ingresos_años[año-1]
+            self.recorrido_anual = millas_años[año-1]
             flujo_neto_actual = self.flujo_neto_anual()
             flujo_neto.append(flujo_neto_actual)
             flujo_descuento_actual = flujo_neto_actual / (1 + self.tasa_descuento) ** año
@@ -105,40 +108,43 @@ class Auto:
             'Ingresos': ingresos_año, 
             'Costos Operativos': costos_año,
             'Flujo Neto': flujo_neto,
-            'Flujo Neto Descontado (10%)': flujo_descuento,
-            'Ingresos Descontado (10%)': ingresos_descuento_año
+            'Flujo Neto Descontado': flujo_descuento,
+            'Ingresos Descontado': ingresos_descuento_año        
         })
+        
         # Calculo el flujo total descontado para el ROI
-        # flujo_total = sum(flujo_descuento)
-        # print(flujo_total)
         return df_flujo_caja
 
     def calcular_roi(self, flujo_total):
-        roi = ((1+ flujo_total/ self.inversion_inicial())**(1/5)-1)*100 #- self.inversion_inicial()
-        return round(roi, 2)
+        roi = flujo_total/ self.inversion_inicial() *100
+        roi_mensual_simple = roi/7/12
+        #roi_mensual_compuesto = ((1+ flujo_total/ self.inversion_inicial())**(1/(7*12))-1)*100 #- self.inversion_inicial()
+        return round(roi, 2),round(roi_mensual_simple, 2)#,round(roi_mensual_compuesto, 2)
 
-    def calcular_ir(self, flujo_total):
-        ir = flujo_total / abs(self.inversion_inicial())
+    def calcular_ir(self, ingreso_total):
+        ir = ingreso_total / abs(self.inversion_inicial())
         return round(ir, 2)
 
     def calcular_payback_period(self):
         flujo_acumulado = 0
         payback = 0
-        for año in range(1, 11):
+        for año in range(1, 8):
             flujo_anual = self.flujo_neto_anual()
             flujo_acumulado += flujo_anual
             if flujo_acumulado >= abs(self.inversion_inicial()):
                 payback = año
                 break
-        return payback if payback > 0 else "No recuperado en 10 años"
+        return payback if payback > 0 else "No recuperado en 7 años"
+    
+#Creación clase AUTO CONVENCIONAL
 
 class AutoConvencional(Auto): #Licencia_1 = unico pago, licencia_2 = anual
-    def __init__(self, precio=36200, iva=0.08875, licencia_1=550,licencia_2=100, placa = 300,
+    def __init__(self, tasa_descuento, precio=36200, iva=0.08875, licencia_1=550,licencia_2=100, placa = 300,
                  seguro=5000, inspeccion = 150,
-                 eficiencia=0.06, mantenimiento=0.15, recorrido_anual=promedio_millas, #mantenimiento = USD x Milla
-                 ingresos_anuales=promedio_ingresos, tasa_descuento=0.10):
-        super().__init__(precio, iva, licencia_1, licencia_2, placa, inspeccion,
-                         seguro, recorrido_anual, ingresos_anuales, tasa_descuento) # Se usa el super constructor para evitar redundancia ya que es una clase hija
+                 eficiencia=25 , mantenimiento=0.15, recorrido_anual=promedio_millas, #eficiencia en MPG / mantenimiento = USD x Milla
+                 ingresos_anuales=promedio_ingresos):
+        super().__init__(tasa_descuento,precio, iva, licencia_1, licencia_2, placa, inspeccion,
+                         seguro, recorrido_anual, ingresos_anuales) # Se usa el super constructor para evitar redundancia ya que es una clase hija
         self.eficiencia = eficiencia
         self.mantenimiento = mantenimiento
     
@@ -146,22 +152,27 @@ class AutoConvencional(Auto): #Licencia_1 = unico pago, licencia_2 = anual
         return self.precio * (1 + self.iva) + self.licencia_1
     
     def costos_operativos(self):
-        precio_gasolina_litro = 0.92 #USD x lt
-        costo_gasolina_anual = (self.recorrido_anual * self.eficiencia) * precio_gasolina_litro
+        precio_gasolina_galon = 3.06 #USD x galon
+        costo_gasolina_anual = (self.recorrido_anual / self.eficiencia) * precio_gasolina_galon
         costo_mantenimiento = self.mantenimiento * self.recorrido_anual
-        costo_anual_salario = 2342 * 21 # Precio medio de la hora de un conductor de autos convencionales.
+        costo_anual_salario = 2342 * 20  # Precio medio de la hora de un conductor de autos convencionales.
         return costo_gasolina_anual + costo_mantenimiento + self.seguro + self.licencia_2 + self.placa + self.inspeccion + costo_anual_salario
 
-class AutoElectrico(Auto): #Descuento de 5% al 10% en licencia ; Excencion del impuesto de venta
-    def __init__(self, precio, eficiencia, iva=0, licencia_1=495,licencia_2=100, placa = 300,
+#Creación Clase AUTO ELÉCTRICO
+
+class AutoElectrico(Auto): #Descuento de 5% al 10% en licencia ; Exencion del impuesto de venta
+    def __init__(self,tasa_descuento, precio, eficiencia, iva=0, licencia_1=495,licencia_2=100, placa = 300,
                  seguro=1500, inspeccion = 100, mantenimiento=0.03, wallbox=600, 
                  recambio_bateria=5000, #mantenimiento = USD x Milla
-                 recorrido_anual=promedio_millas, ingresos_anuales=promedio_ingresos, tasa_descuento=0.10): #16529 #93227
-        super().__init__(precio, iva, licencia_1, licencia_2, placa, inspeccion, seguro, recorrido_anual, ingresos_anuales, tasa_descuento)
+                 recorrido_anual=promedio_millas, ingresos_anuales=promedio_ingresos): 
+        super().__init__(tasa_descuento, precio, iva, licencia_1, licencia_2, placa, inspeccion, seguro, recorrido_anual, 
+                         ingresos_anuales)
         self.eficiencia = eficiencia
         self.mantenimiento = mantenimiento
         self.wallbox = wallbox
         self.recambio_bateria = recambio_bateria
+        self.tasa_descuento = tasa_descuento
+        self.licencia_1=licencia_1
     
     def inversion_inicial(self):
         return self.precio + self.licencia_1 + self.wallbox
@@ -171,35 +182,38 @@ class AutoElectrico(Auto): #Descuento de 5% al 10% en licencia ; Excencion del i
         costo_electricidad_anual = (self.recorrido_anual * self.eficiencia) * precio_electricidad_kWh
         costo_mantenimiento = self.mantenimiento * self.recorrido_anual
         costo_anual_bateria = self.recambio_bateria / 12.5
-        costo_anual_salario = 2342 * 22.5 # Promedio de horas de menejo de un conductor por año * Precio promedio de la hora de un chofer de taxis en NY.
+        costo_anual_salario = 2342 * 22 # Promedio de horas de menejo de un conductor por año * Precio promedio de la hora de un chofer de taxis en NY para VE.
         return costo_electricidad_anual + costo_mantenimiento + costo_anual_bateria + self.seguro + self.licencia_2 + self.placa + self.inspeccion + costo_anual_salario
 
-def calcular_metricas_flota(cantidad_ve, cantidad_conv):
+def calcular_metricas_flota(tasa_descuento, cantidad_ve, cantidad_conv):
     resultados = []
-
+    flujo_neto_comb_lista = []
     for _, fila in df_autos.iterrows():
         # Se extraen los datos del Dataset
+        marca = fila['Brand']
         modelo = fila['Model']
         precio_ve = fila['Precio Dolar']
         eficiencia_ve = fila['Efficiency (kWh/mile)']
         
         # Se instancian los autos segun su naturaleza (electrico o convencional)
-        auto_electrico = AutoElectrico(precio=precio_ve, eficiencia=eficiencia_ve)
-        auto_convencional = AutoConvencional()
-        
+        auto_electrico = AutoElectrico(tasa_descuento, precio=precio_ve, eficiencia=eficiencia_ve)
+        auto_convencional = AutoConvencional(tasa_descuento)
+
+
         # Se calcula el flujo de caja proyectado para cada tipo de auto
         flujo_ve = auto_electrico.flujo_caja_proyectado()
         flujo_conv = auto_convencional.flujo_caja_proyectado()
-        
+        #print (flujo_ve)
+        flujo_neto_comb_lista =  cantidad_ve * flujo_ve['Flujo Neto Descontado'] +  cantidad_conv  * flujo_conv['Flujo Neto Descontado']
         # Se combinar flujos de caja para la flota (teniendo en cuenta su respectiva cantidad)
         flujo_neto_comb = round(
-            cantidad_ve * flujo_ve['Flujo Neto Descontado (10%)'].sum() +
-            cantidad_conv * flujo_conv['Flujo Neto Descontado (10%)'].sum(), 2
+            cantidad_ve * flujo_ve['Flujo Neto Descontado'].sum() +
+            cantidad_conv * flujo_conv['Flujo Neto Descontado'].sum(), 2
         )
         
         ingreso_comb =  round(
-            cantidad_ve * flujo_ve['Ingresos Descontado (10%)'].sum() +
-            cantidad_conv * flujo_conv['Ingresos Descontado (10%)'].sum(), 2
+            cantidad_ve * flujo_ve['Ingresos Descontado'].sum() +
+            cantidad_conv * flujo_conv['Ingresos Descontado'].sum(), 2
         )
 
         # Se calcula  la inversión inicial combinada
@@ -209,9 +223,11 @@ def calcular_metricas_flota(cantidad_ve, cantidad_conv):
         )
         
         # Se calcular las métricas financieras combinadas
-        roi_comb = ((1+flujo_neto_comb/inversion_comb)**(1/5)-1)*100
+        roi_comb = flujo_neto_comb/ inversion_comb *100
+        roi_mensual_simple = roi_comb/7/12
         ir_comb = ingreso_comb / abs(inversion_comb)
-        
+        tir_comb = round(npf.irr(flujo_neto_comb_lista)*100,2)
+
         # Calculo del payback period combinado (considerando la proporción)
         flujo_anual_comb = [
             cantidad_ve * flujo_ve['Flujo Neto'].iloc[año] +
@@ -228,12 +244,18 @@ def calcular_metricas_flota(cantidad_ve, cantidad_conv):
         
         # Se almacenan los resultados para el modelo actual
         resultados.append({
-            'Modelo': modelo,
-            'Flujo Neto Total (USD/año)': round(flujo_neto_comb ,2),
-            'ROI (%)': round(roi_comb, 2),
-            'IR (USD)': round(ir_comb, 2),
-            'Payback Period (años)': payback_comb if payback_comb > 0 else "No recuperado en 10 años"
+            'Brand': marca,
+            'Model': modelo,
+            'Efficiency (kWh/mile)' : eficiencia_ve,
+            'Inversión Inicial Total (USD)': inversion_comb,
+            'VNA (USD)': round(flujo_neto_comb,1),
+            'TIR (%)':tir_comb,
+            'ROI (%)': roi_comb,
+            'ROI Mensualizado(%)': roi_mensual_simple,
+            'IR (USD)': ir_comb,
+            'Payback Period (Años)': payback_comb if payback_comb > 0 else "No recuperado en 7 años"
         })
+
 
     # Se convertierten los resultados a tipo DataFrame para una mejor visualización
     df_resultados = pd.DataFrame(resultados)
@@ -242,18 +264,16 @@ def calcular_metricas_flota(cantidad_ve, cantidad_conv):
 
     return df_resultados
 
-# Crear un selector para la industria
+tasa_descuento = 0.10 # Primer Parámetro : Cantidad VE; Segundo Parámetro : Cantidad Autos Convencional
+# Crear un selector para la cantidad de autos
 cantidad_ve = st.slider("Seleccione cantidad de autos eléctricos:",min_value=0, max_value=1500, step=1)
 cantidad_conv = st.slider("Seleccione cantidad de autos convencionales:",min_value=0, max_value=1500, step=1)
-
-resultados_flota = calcular_metricas_flota(cantidad_ve,cantidad_conv)
-
-
+resultados_flota = calcular_metricas_flota(tasa_descuento, cantidad_ve, cantidad_conv)
 # Mostrar el DataFrame resultante
 st.write("Tabla de Resultados:")
 st.markdown(f"""
 En esta tabla se muestran los 5 mejores modelos de vehículos eléctricos en función a su rentabilidad.
-A su vez se calculan distintas métricas como ROI%, IR(USD), Payback Period (Años) y Flujo Neto Total (USD) para la combinación de {cantidad_ve} vehículos eléctricos y {cantidad_conv} vehículos convencionales. 
+A su vez se calculan distintas métricas como Efficiency (kWh/mile), Inversión Inicial Total (USD), VNA(USD), TIR (%), ROI%, ROI% mensualizado, IR(USD) y Payback Period (Años) para la combinación de {cantidad_ve} vehículos eléctricos y {cantidad_conv} vehículos convencionales. 
             """)
 # Mostrar el DataFrame resultante
 df5 = resultados_flota.head(5) 
